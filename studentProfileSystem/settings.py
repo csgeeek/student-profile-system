b6b4995f274b8cb8864bc1e2d6e7c61fdaa435c2
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-09-01 23:39:31 +0530

    setup mysql database

--- studentProfileSystem/settings.py
+++ studentProfileSystem/settings.py
@@ -80,11 +80,19 @@ WSGI_APPLICATION = 'studentProfileSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student_profile_system',
+        'USER': 'root',
+        'PASSWORD': 'Mysql@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
+# 'default': {
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
